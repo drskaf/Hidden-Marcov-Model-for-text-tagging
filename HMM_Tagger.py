9e9e0857dc723e859f0bddd1b77060c4cb168f03
Ebraham <73792019+drskaf@users.noreply.github.com>
2022-06-01 10:39:43 +0100

    Update HMM_Tagger.py

--- HMM_Tagger.py
+++ HMM_Tagger.py
@@ -7,3 +7,12 @@ from itertools import chain
 from collections import Counter, defaultdict
 from helpers import show_model, Dataset
 from pomegranate import State, HiddenMarkovModel, DiscreteDistribution
+
+data = Dataset("NAME_OF_TAGS_DATA", "NAME_OF_DATA_FILE.txt", train_test_split=0.8)
+
+print("There are {} sentences in the corpus.".format(len(data)))
+print("There are {} sentences in the training set.".format(len(data.training_set)))
+print("There are {} sentences in the testing set.".format(len(data.testing_set)))
+
+assert len(data) == len(data.training_set) + len(data.testing_set), \
+       "The number of sentences in the training set + testing set should sum to the number of sentences in the corpus"
