c5e8e028bab4addb589d8ff0c446f5b9c5100bb6
Ebraham <73792019+drskaf@users.noreply.github.com>
2022-06-02 09:52:26 +0100

    Update HMM_Tagger.py

--- HMM_Tagger.py
+++ HMM_Tagger.py
@@ -16,3 +16,33 @@ print("There are {} sentences in the testing set.".format(len(data.testing_set))
 
 assert len(data) == len(data.training_set) + len(data.testing_set), \
        "The number of sentences in the training set + testing set should sum to the number of sentences in the corpus"
+
+key = 'KEY-TO-SENTENCE'
+print("Sentence: {}".format(key))
+print("words:\n\t{!s}".format(data.sentences[key].words))
+print("tags:\n\t{!s}".format(data.sentences[key].tags))
+
+print("There are a total of {} samples of {} unique words in the corpus."
+      .format(data.N, len(data.vocab)))
+print("There are {} samples of {} unique words in the training set."
+      .format(data.training_set.N, len(data.training_set.vocab)))
+print("There are {} samples of {} unique words in the testing set."
+      .format(data.testing_set.N, len(data.testing_set.vocab)))
+print("There are {} words in the test set that are missing in the training set."
+      .format(len(data.testing_set.vocab - data.training_set.vocab)))
+
+assert data.N == data.training_set.N + data.testing_set.N, \
+       "The number of training + test samples should sum to the total number of samples"
+
+# accessing words with Dataset.X and tags with Dataset.Y 
+for i in range(2):    
+    print("Sentence {}:".format(i + 1), data.X[i])
+    print()
+    print("Labels {}:".format(i + 1), data.Y[i])
+    print()
+       
+# use Dataset.stream() (word, tag) samples for the entire corpus
+print("\nStream (word, tag) pairs:\n")
+for i, pair in enumerate(data.stream()):
+    print("\t", pair)
+    if i > 5: break
