000a09bff84d64a2a8c608c6f92ffe481e1ed0e6
Ebraham <73792019+drskaf@users.noreply.github.com>
2022-06-10 18:51:06 +0100

    Update HMM_Tagger.py

--- HMM_Tagger.py
+++ HMM_Tagger.py
@@ -228,3 +228,51 @@ assert min(tag_bigrams, key=tag_bigrams.get) in [('X', 'NUM'), ('PRON', 'X')], \
 assert max(tag_bigrams, key=tag_bigrams.get) in [('DET', 'NOUN')], \
        "Hmmm...('DET', 'NOUN') is expected to be the most common bigram."
 HTML('<div class="alert alert-block alert-success">Your tag bigrams look good!</div>')
+
+# Define starting counts
+def starting_counts(sequences):
+    """Return a dictionary keyed to each unique value in the input sequences list
+    that counts the number of occurrences where that value is at the beginning of
+    a sequence.
+    
+    For example, if 8093 sequences start with NOUN, then you should return a
+    dictionary such that your_starting_counts[NOUN] == 8093
+    """
+    counts = defaultdict(int)
+    for sequence in sequences:
+        counts[sequence[0]] += 1
+    return counts
+
+    raise NotImplementedError
+
+# Calculate the count of each tag starting a sequence
+tag_starts = starting_counts(data.training_set.Y)
+
+assert len(tag_starts) == 12, "Uh oh. There should be 12 tags in your dictionary."
+assert min(tag_starts, key=tag_starts.get) == 'X', "Hmmm...'X' is expected to be the least common starting bigram."
+assert max(tag_starts, key=tag_starts.get) == 'DET', "Hmmm...'DET' is expected to be the most common starting bigram."
+HTML('<div class="alert alert-block alert-success">Your starting tag counts look good!</div>')
+
+# Define ending counts
+def ending_counts(sequences):
+    """Return a dictionary keyed to each unique value in the input sequences list
+    that counts the number of occurrences where that value is at the end of
+    a sequence.
+    
+    For example, if 18 sequences end with DET, then you should return a
+    dictionary such that your_starting_counts[DET] == 18
+    """
+    counts = defaultdict(int)
+    for sequence in sequences:
+        counts[sequence[-1]] += 1
+    return counts
+
+    raise NotImplementedError
+
+# Calculate the count of each tag ending a sequence
+tag_ends = ending_counts(data.training_set.Y)
+
+assert len(tag_ends) == 12, "Uh oh. There should be 12 tags in your dictionary."
+assert min(tag_ends, key=tag_ends.get) in ['X', 'CONJ'], "Hmmm...'X' or 'CONJ' should be the least common ending bigram."
+assert max(tag_ends, key=tag_ends.get) == '.', "Hmmm...'.' is expected to be the most common ending bigram."
+HTML('<div class="alert alert-block alert-success">Your ending tag counts look good!</div>')
